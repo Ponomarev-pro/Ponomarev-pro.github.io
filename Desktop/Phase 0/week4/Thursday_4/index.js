26f0e390b6395f55ab8da2f6a162e9ab14615616
Даниил Пономарев <daniil@MacBookPro.lan>
2024-12-08 22:10:47 +0300

    hm

---
+++ Desktop/Phase 0/week4/Thursday_4/index.js
@@ -0,0 +1,7 @@
+function isInRange (num, minValue, maxValue) {
+    if(num >= minValue && num <= maxValue){
+        return ('true')
+    }else{
+        return ('false')
+    }
+}
---
+++ Desktop/Phase 0/week4/Thursday_4/index3.js
@@ -0,0 +1,5 @@
+function isVowel(let) {
+    const letters = ["а", "А", "е", "Е", "ё", "Ё", "и", "И", "о", "О", "у", "У", "ы", "Ы", "Ю", "Э", "Я", "ю", "э", "я", "a", "A", "e", "E", "i", "I", "o", "O", "y", "Y", "U", "u"];
+    const result = letters.includes(let)
+    return result
+}
